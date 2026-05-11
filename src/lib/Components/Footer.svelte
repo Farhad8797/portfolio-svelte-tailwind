90c18bc7ffaa70956e86e5fe49f9a25a1766c806
Farhad8797 <gdvbhbhb@gmail.com>
2026-05-12 00:31:11 +0600

    Email fix

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -1,14 +1,15 @@
 <script>
     let contactInfo = [
-        {link:"mailto:gdvbhbhb@gmail.com?subject=Inquiry from Portfolio", icon:"fa-solid fa-envelope", name:"Email"},
         {link:"https://www.facebook.com/profile.php?id=100067398461657", icon:"fa-brands fa-facebook", name: "Facebook"},
         {link:"https://github.com/Farhad8797/", icon:"fa-solid fa-code-branch", name:"Github"},
         {link:"https://leetcode.com/u/Md_Farhad_Hossain/", icon:"fa-solid fa-code", name:"Leetcode"}
     ]
 </script>
 <h1 class="text-center text-3xl lg:text-4xl py-5">Contact Me</h1>
-<section id="Contact Me" class="flex items-center gap-5 lg:gap-8 justify-center p-10">
+<section id="Contact Me" class="flex items-center gap-5 lg:gap-8 justify-center p-5">
+    <p class="text-2xl">Check out my profiles:</p>
     {#each contactInfo as {link, icon, name}}
         <a href={link} target="_blank" aria-label={name} class="text-2xl lg:text-3xl hover:text-yellow-400 transition duration-500"><span><i class={icon}></i></span></a>
     {/each}
 </section>
+<h1 class="text-center p-5 text-2xl">Mail me: <span class="italic">gdvbhbhb@gmail.com</span></h1>
